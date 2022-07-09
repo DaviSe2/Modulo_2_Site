ae9e6c307a1e80f0467442bffa31660916b0024c
Davi de Sena Eduardo <davidesenaeduardo@gmail.com>
2022-07-09 20:23:26 -0300

    Final

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
 
+    // Seletor da promoção
+
+    $('.promoSelect').on('click', function(){
+
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+    
+        $('.promoSelect').removeClass('active');
+        $(this).addClass('active');
+    
+       });
+
     // Progress bar
     let containerA = document.getElementById("circleA");
 
     let circleA = new ProgressBar.Circle(containerA,{
-        color:'#64DAF9',
+        color:'#FFF',
         strokeWidth: 8,
         duration: 1400,
         from: {color:'#AAA'},
-        to: {color:'#65DAf9'},
+        to: {color:'#852E7C'},
 
         step: function(state, circle){
 
             circle.path.setAttribute('stroke', state.color);
 
-            let value = Math.round(circle.value() * 60);
+            let value = Math.round(circle.value() * 5786);
 
             circle.setText(value);
 
@@ -25,17 +37,17 @@ $(document).ready(function(){
     let containerB = document.getElementById("circleB");
 
     let circleB = new ProgressBar.Circle(containerB,{
-        color:'#64DAF9',
+        color:'#FFF',
         strokeWidth: 8,
         duration: 1600,
         from: {color:'#AAA'},
-        to: {color:'#65DAf9'},
+        to: {color:'#852E7C'},
 
         step: function(state, circle){
 
             circle.path.setAttribute('stroke', state.color);
 
-            let value = Math.round(circle.value() * 127);
+            let value = Math.round(circle.value() * 892);
 
             circle.setText(value);
 
@@ -46,17 +58,17 @@ $(document).ready(function(){
     let containerC = document.getElementById("circleC");
 
     let circleC = new ProgressBar.Circle(containerC,{
-        color:'#64DAF9',
+        color:'#FFF',
         strokeWidth: 8,
         duration: 1800,
         from: {color:'#AAA'},
-        to: {color:'#65DAf9'},
+        to: {color:'#852E7C'},
 
         step: function(state, circle){
 
             circle.path.setAttribute('stroke', state.color);
 
-            let value = Math.round(circle.value() * 254);
+            let value = Math.round(circle.value() * 157);
 
             circle.setText(value);
 
@@ -87,9 +99,89 @@ $(document).ready(function(){
     setTimeout(function(){
         
         $('#satisfacao-area').parallax({imageSrc: 'imagens/parallax.jpg'});
+        $('#roteiros-area').parallax({imageSrc: 'imagens/padraoParallax.png'});
 
-    }, 250);
+    },250);
 
-   
+   // Filtro da Galeria
+
+   $('.filterSelect').on('click', function(){
+
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+
+    $('.filterSelect').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'sw-btn'){
+        eachBoxes('sw',boxes)
+    } else if(type == 'got-btn'){
+        eachBoxes('got',boxes)
+    } else if(type == 'hp-btn'){
+        eachBoxes('hp',boxes)
+    }else{
+        eachBoxes('all',boxes)
+    }
+
+   });
+
+   function eachBoxes(type, boxes){
+    if(type == 'all'){
+        $(boxes).fadeIn();
+    } else {
+        $(boxes).each(function(){
+            if(!$(this).hasClass(type)){
+                $(this).fadeOut('slow');
+            }else{
+                $(this).fadeIn();
+            }
+        });
+    }
+
+   }
+
+   // scroll para seções
+
+    let navBtn = $('.nav-item');
+
+   let bannerSection = $('#mainSlider');
+   let promocaoSection = $('#promocao-area');
+   let destinosSection = $('#destinos-area');
+   let roteirosSection = $('#roteiros-area');
+   let contatoSection = $('#contato-area');
+
+   let scrollTo = '';
+
+   $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    console.log(btnId);
+
+    if(btnId == 'promocao-menu') {
+        scrollTo = promocaoSection;
+    } else if(btnId == 'destinos-menu') {
+        scrollTo = destinosSection;
+    } else if(btnId == 'roteiros-menu'){
+        scrollTo = roteirosSection;
+    } else if(btnId == 'contato-menu') {
+        scrollTo = contatoSection;
+    } else {
+        scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(scrollTo).offset().top -70 
+    }, 1000);
+
+   });
+
+   let roteirosBtn = $('#roteiros-btn');
+
+   $(roteirosBtn).click(function(){
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $('#contato-area').offset().top -70
+    }, 1000);
+   });
 
 });
